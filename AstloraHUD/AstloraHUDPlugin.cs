18de991d8b58c16bcd49883d900cc0192d7c8279
Felix Liew <changqing.se@gmail.com>
2026-05-29 05:30:44 +0800

    Defective Update v1.0.2
    Basic functionality is working in this version, though there is still a known issue related to URL-based images in the configuration file.

--- AstloraHUD/AstloraHUDPlugin.cs
+++ AstloraHUD/AstloraHUDPlugin.cs
@@ -1,10 +1,11 @@
 ﻿using Rocket.Core;
-using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
+using Rocket.Unturned.Chat; 
 using Rocket.Unturned.Player;
 using SDG.Unturned;
-using System.Timers;
+using System.Collections;
+using UnityEngine;
 
 namespace FelixLiew.AstloraHUD
 {
@@ -12,145 +13,114 @@ namespace FelixLiew.AstloraHUD
     {
         private const ushort EffectId = 32502;
         private const short EffectKey = 301;
-        private Timer timer;
+        private Coroutine _uiUpdateCoroutine;
+
         protected override void Load()
         {
             U.Events.OnPlayerConnected += OnPlayerConnected;
-            U.Events.OnPlayerDisconnected += OnPlayerDisconnected;
             EffectManager.onEffectButtonClicked += OnEffectButtonClicked;
-            Logger.Log($"{Name} {Assembly.GetName().Version.ToString(3)} has been loaded!");
 
-            timer = new Timer(60000);
-            timer.Elapsed += (sender, args) =>
-            {
-                UpdateTimeUI();
-            };
-            timer.Start();
+            _uiUpdateCoroutine = StartCoroutine(UpdateUILoop());
+
+            Rocket.Core.Logging.Logger.Log($"{Name} {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(3)} has been loaded!");
         }
+
         protected override void Unload()
         {
             U.Events.OnPlayerConnected -= OnPlayerConnected;
-            U.Events.OnPlayerDisconnected -= OnPlayerDisconnected;
             EffectManager.onEffectButtonClicked -= OnEffectButtonClicked;
-            timer?.Stop();
-            timer?.Dispose();
-            Logger.Log($"{Name} has been unloaded!");
+
+            if (_uiUpdateCoroutine != null)
+            {
+                StopCoroutine(_uiUpdateCoroutine);
+            }
+
+            Rocket.Core.Logging.Logger.Log($"{Name} has been unloaded!");
         }
 
         private void OnPlayerConnected(UnturnedPlayer player)
         {
             EffectManager.sendUIEffect(EffectId, EffectKey, player.CSteamID, true);
-            this.UpdatePlayersUI();
-            this.UpdateTimeUI();
+
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "Logo", Configuration.Instance.LogoUrl);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "Polosa", Configuration.Instance.PolosaUrl);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "R1", Configuration.Instance.R1Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "R2", Configuration.Instance.R2Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "R3", Configuration.Instance.R3Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "R4", Configuration.Instance.R4Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "L1", Configuration.Instance.L1Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "L2", Configuration.Instance.L2Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "L3", Configuration.Instance.L3Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "L4", Configuration.Instance.L4Url);
+            EffectManager.sendUIEffectImageURL(EffectKey, player.CSteamID, true, "L5", Configuration.Instance.L5Url);
+
+            EffectManager.sendUIEffectText(EffectKey, player.CSteamID, true, "Name", Configuration.Instance.ServerNameText);
+            EffectManager.sendUIEffectText(EffectKey, player.CSteamID, true, "Description", Configuration.Instance.ServerDescriptionText);
         }
-        private void OnPlayerDisconnected(UnturnedPlayer player)
+
+        private IEnumerator UpdateUILoop()
         {
-            this.UpdatePlayersUI();
-        }
-        private void UpdatePlayersUI()
-        {
-            foreach (SteamPlayer steamPlayer in Provider.clients)
+            while (true)
             {
-                EffectManager.sendUIEffectText(EffectKey, steamPlayer.transportConnection, true, "Players", string.Format("{0}/{1}", Provider.clients.Count, Provider.maxPlayers));
-            }
-        }
+                yield return new WaitForSeconds(1f);
 
-        private void UpdateTimeUI()
-        {
-            uint time = LightingManager.time;
+                float timeFraction = (float)LightingManager.time / (float)LightingManager.cycle;
+                int hours = (int)(timeFraction * 24);
+                int minutes = (int)((timeFraction * 24 - hours) * 60);
 
-            string formattedTime = $"{time / 100:D2}:{time % 100:D2}";
+                string gameTimeText = $"{hours:00}:{minutes:00}";
+                string playersText = $"{Provider.clients.Count}/{Provider.maxPlayers}";
 
-            foreach (SteamPlayer steamPlayer in Provider.clients)
-            {
-                EffectManager.sendUIEffectText(
-                    EffectKey,
-                    steamPlayer.transportConnection,
-                    true,
-                    "Time",
-                    formattedTime
-                );
+                foreach (SteamPlayer steamPlayer in Provider.clients)
+                {
+                    EffectManager.sendUIEffectText(EffectKey, steamPlayer.transportConnection, true, "Players", playersText);
+                    EffectManager.sendUIEffectText(EffectKey, steamPlayer.transportConnection, true, "Time", gameTimeText);
+                }
             }
         }
         private void OnEffectButtonClicked(Player player, string buttonName)
         {
+            UnturnedPlayer uPlayer = UnturnedPlayer.FromPlayer(player);
+            if (uPlayer == null) return;
+
             switch (buttonName)
             {
-                case "R1":
-                    R1(player);
+                case "R1": ExecuteButtonAction(uPlayer, Configuration.Instance.R1Type, Configuration.Instance.R1Value, Configuration.Instance.R1Desc); break;
+                case "R2": ExecuteButtonAction(uPlayer, Configuration.Instance.R2Type, Configuration.Instance.R2Value, Configuration.Instance.R2Desc); break;
+                case "R3": ExecuteButtonAction(uPlayer, Configuration.Instance.R3Type, Configuration.Instance.R3Value, Configuration.Instance.R3Desc); break;
+                case "R4": ExecuteButtonAction(uPlayer, Configuration.Instance.R4Type, Configuration.Instance.R4Value, Configuration.Instance.R4Desc); break;
+                case "L1": ExecuteButtonAction(uPlayer, Configuration.Instance.L1Type, Configuration.Instance.L1Value, Configuration.Instance.L1Desc); break;
+                case "L2": ExecuteButtonAction(uPlayer, Configuration.Instance.L2Type, Configuration.Instance.L2Value, Configuration.Instance.L2Desc); break;
+                case "L3": ExecuteButtonAction(uPlayer, Configuration.Instance.L3Type, Configuration.Instance.L3Value, Configuration.Instance.L3Desc); break;
+                case "L4": ExecuteButtonAction(uPlayer, Configuration.Instance.L4Type, Configuration.Instance.L4Value, Configuration.Instance.L4Desc); break;
+                case "L5": ExecuteButtonAction(uPlayer, Configuration.Instance.L5Type, Configuration.Instance.L5Value, Configuration.Instance.L5Desc); break;
+            }
+        }
+        private void ExecuteButtonAction(UnturnedPlayer player, ButtonFunctionType type, string value, string desc)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            switch (type)
+            {
+                case ButtonFunctionType.Web:
+                    player.Player.sendBrowserRequest(desc, value);
                     break;
 
-                case "R2":
-                    R2(player);
+                case ButtonFunctionType.Command:
+                    R.Commands.Execute(player, value);
                     break;
 
-                case "R3":
-                    R3(player);
+                case ButtonFunctionType.Message:
+                    UnturnedChat.Say(player, value, Color.green);
                     break;
-                case "R4":
-                    R4(player);
-                    break;
-
-                case "L1":
-                    L1(player);
-                    break;
-                case "L2":
-                    L2(player);
-                    break;
-                case "L3":
-                    L3(player);
-                    break;
-                case "L4":
-                    L4(player);
-                    break;
-                case "L5":
-                    L5(player);
+                case ButtonFunctionType.UIEffect:
+        
+                    if (ushort.TryParse(value, out ushort uiId))
+                    {         
+                        EffectManager.sendUIEffect(uiId, (short)uiId, player.CSteamID, true);
+                    }
                     break;
             }
         }
-
-        private void R1(Player player)
-        {
-            player.sendBrowserRequest("欢迎访问星洛拉官网!", "https://rp.astlora.cn");
-        }
-
-        private void R2(Player player)
-        {
-            player.sendBrowserRequest("欢迎加入我们的社群!", "https://qm.qq.com/q/5TQ0da0gXC");
-        }
-
-        private void R3(Player player)
-        {
-            player.sendBrowserRequest("欢迎加入我们的论坛!", "https://un.astlora.cn");
-        }
-        private void R4(Player player)
-        {
-            player.sendBrowserRequest("欢迎访问星洛云官网!", "https://astlora.cn");
-        }
-        private void L1(Player player)
-        {
-            UnturnedPlayer uPlayer = UnturnedPlayer.FromPlayer(player);
-            R.Commands.Execute(uPlayer, "akit");
-        }
-        private void L2(Player player)
-        {
-            UnturnedPlayer uPlayer = UnturnedPlayer.FromPlayer(player);
-            R.Commands.Execute(uPlayer, "go");
-        }
-        private void L3(Player player)
-        {
-            UnturnedPlayer uPlayer = UnturnedPlayer.FromPlayer(player);
-            R.Commands.Execute(uPlayer, "ranking");
-        }
-        private void L4(Player player)
-        {
-            player.sendBrowserRequest("星落拉·纯净工艺 支持中心!", "https://rp.astlora.cn/submitticket");
-        }
-        private void L5(Player player)
-        {
-            UnturnedPlayer uPlayer = UnturnedPlayer.FromPlayer(player);
-            R.Commands.Execute(uPlayer, "music");
-        }
-
     }
 }
